d23f3a8a4eda783baebe09a248719e410611b0db
mcgear <michael.gearhardt@gmail.com>
2024-01-10 10:30:02 -0700

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,5 +1,5 @@
 export { type JSX } from "preact";
-export * from "https://deno.land/x/fathym_common@v0.0.120/mod.ts";
+export * from "https://deno.land/x/fathym_common@v0.0.131/mod.ts";
 export * from "$fresh/server.ts";
 export * from "$fresh/session";
 export * as msal from "npm:@azure/msal-node@2.1.0";
