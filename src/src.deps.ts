607affb2e93cbee81a50f81a59d2c2884d18a2cc
mcgear <michael.gearhardt@gmail.com>
2024-08-20 13:05:13 -0600

    direct versions

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,12 +1,12 @@
-export * from "jsr:@fathym/common@0";
+export * from "jsr:@fathym/common@0.2.22";
 
-export * from "jsr:@fathym/eac@0";
-export * from "jsr:@fathym/eac@0/applications";
-export * from "jsr:@fathym/eac@0/identity";
+export * from "jsr:@fathym/eac@0.1.18";
+export * from "jsr:@fathym/eac@0.1.18/applications";
+export * from "jsr:@fathym/eac@0.1.18/identity";
 
-export * from "jsr:@fathym/eac-runtime@0";
+export * from "jsr:@fathym/eac-runtime@0.1.7";
 
-export * from "jsr:@fathym/ioc@0";
+export * from "jsr:@fathym/ioc@0.0.12";
 
 export * as msal from "npm:@azure/msal-node@2.1.0";
 export * from "npm:@azure/msal-node@2.1.0";
