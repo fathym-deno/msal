b5c5fa31770f678cf5267ec2d8272a8beb82e196
mcgear <michael.gearhardt@gmail.com>
2025-02-18 17:24:48 -0700

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,20 +1,20 @@
-export * from "jsr:@fathym/common@0.2.175";
-export * from "jsr:@fathym/common@0.2.175/log";
+export * from "jsr:@fathym/common@0.2.178";
+export * from "jsr:@fathym/common@0.2.178/log";
 
-export * from "jsr:@fathym/eac@0.2.36";
-export * from "jsr:@fathym/eac@0.2.36/runtime";
-export * from "jsr:@fathym/eac@0.2.36/runtime/config";
-export * from "jsr:@fathym/eac@0.2.36/runtime/pipelines";
-export * from "jsr:@fathym/eac@0.2.36/runtime/plugins";
+export * from "jsr:@fathym/eac@0.2.78";
+export * from "jsr:@fathym/eac@0.2.78/runtime";
+export * from "jsr:@fathym/eac@0.2.78/runtime/config";
+export * from "jsr:@fathym/eac@0.2.78/runtime/pipelines";
+export * from "jsr:@fathym/eac@0.2.78/runtime/plugins";
 
-export * from "jsr:@fathym/eac-applications@0.0.54/processors";
-export * from "jsr:@fathym/eac-applications@0.0.54/runtime/processors";
+export * from "jsr:@fathym/eac-applications@0.0.78/processors";
+export * from "jsr:@fathym/eac-applications@0.0.78/runtime/processors";
 
-export * from "jsr:@fathym/eac-identity@0.0.6";
+export * from "jsr:@fathym/eac-identity@0.0.8";
 
-export * from "jsr:@fathym/ioc@0.0.13";
+export * from "jsr:@fathym/ioc@0.0.14";
 
-export * as msal from "npm:@azure/msal-node@2.1.0";
-export * from "npm:@azure/msal-node@2.1.0";
+export * as msal from "npm:@azure/msal-node@2.16.2";
+export * from "npm:@azure/msal-node@2.16.2";
 export { type AccessToken } from "npm:@azure/identity@2.1.0";
 export * as ArmResource from "npm:@azure/arm-subscriptions@5.1.0";
