54ec14d8ec80e4ebce27f387ad31733fd9899ee8
mcgear <michael.gearhardt@gmail.com>
2025-02-25 11:37:32 -0700

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,16 +1,16 @@
 export * from "jsr:@fathym/common@0.2.179";
 export * from "jsr:@fathym/common@0.2.179/log";
 
-export * from "jsr:@fathym/eac@0.2.90";
-export * from "jsr:@fathym/eac@0.2.90/runtime";
-export * from "jsr:@fathym/eac@0.2.90/runtime/config";
-export * from "jsr:@fathym/eac@0.2.90/runtime/pipelines";
-export * from "jsr:@fathym/eac@0.2.90/runtime/plugins";
+export * from "jsr:@fathym/eac@0.2.91";
+export * from "jsr:@fathym/eac@0.2.91/runtime";
+export * from "jsr:@fathym/eac@0.2.91/runtime/config";
+export * from "jsr:@fathym/eac@0.2.91/runtime/pipelines";
+export * from "jsr:@fathym/eac@0.2.91/runtime/plugins";
 
-export * from "jsr:@fathym/eac-applications@0.0.84/processors";
-export * from "jsr:@fathym/eac-applications@0.0.84/runtime/processors";
+export * from "jsr:@fathym/eac-applications@0.0.86/processors";
+export * from "jsr:@fathym/eac-applications@0.0.86/runtime/processors";
 
-export * from "jsr:@fathym/eac-identity@0.0.10";
+export * from "jsr:@fathym/eac-identity@0.0.11";
 
 export * from "jsr:@fathym/ioc@0.0.14";
 
