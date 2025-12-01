96c583683f9b32af8aeb68e3545f30e67f8ee0f6
mcgear <michael.gearhardt@gmail.com>
2025-12-01 12:03:17 -0700

    ref updates

--- src/.deps.ts
+++ src/.deps.ts
@@ -1,30 +1,30 @@
 export { redirectRequest } from "jsr:@fathym/common@0.2.299/http";
 export { telemetryFor } from "jsr:@fathym/common@0.2.299/telemetry";
 
-export { type EverythingAsCode } from "jsr:@fathym/eac@0.2.157-hmis";
-export { type EaCRuntimeContext } from "jsr:@fathym/eac@0.2.157-hmis/runtime";
+export { type EverythingAsCode } from "jsr:@fathym/eac@0.2.163-hmis";
+export { type EaCRuntimeContext } from "jsr:@fathym/eac@0.2.163-hmis/runtime";
 export {
   type EaCRuntimeConfig,
   type EaCRuntimePluginConfig,
-} from "jsr:@fathym/eac@0.2.157-hmis/runtime/config";
+} from "jsr:@fathym/eac@0.2.163-hmis/runtime/config";
 export {
   type EaCRuntimeHandler,
   type EaCRuntimeHandlers,
-} from "jsr:@fathym/eac@0.2.157-hmis/runtime/pipelines";
-export { type EaCRuntimePlugin } from "jsr:@fathym/eac@0.2.157-hmis/runtime/plugins";
+} from "jsr:@fathym/eac@0.2.163-hmis/runtime/pipelines";
+export { type EaCRuntimePlugin } from "jsr:@fathym/eac@0.2.163-hmis/runtime/plugins";
 export {
   type EaCAzureADProviderDetails,
   type EverythingAsCodeIdentity,
-} from "jsr:@fathym/eac-identity@0.0.37-integration";
+} from "jsr:@fathym/eac-identity@0.0.42-integration";
 
 export {
   type EaCApplicationProcessorConfig,
   type EaCProcessor,
   isEaCProcessor,
-} from "jsr:@fathym/eac-applications@0.0.251-mcp-processor/processors";
+} from "jsr:@fathym/eac-applications@0.0.263-mcp-processor/processors";
 export {
   type ProcessorHandlerResolver,
-} from "jsr:@fathym/eac-applications@0.0.251-mcp-processor/runtime/processors";
+} from "jsr:@fathym/eac-applications@0.0.263-mcp-processor/runtime/processors";
 
 export { IoCContainer } from "jsr:@fathym/ioc@0.0.21";
 
