0b95e34eb88162348dccc3fad9ec3319d8a53ea8
Michael Gearhardt <michael.gearhardt@fathym.com>
2025-12-12 10:09:09 -0700

    chore(msal): release build for version update cascade

--- src/.deps.ts
+++ src/.deps.ts
@@ -1,33 +1,33 @@
-export { redirectRequest } from "jsr:@fathym/common@0.2.306/http";
-export { telemetryFor } from "jsr:@fathym/common@0.2.306/telemetry";
+export { redirectRequest } from "jsr:@fathym/common@0.2.307-common-release/http";
+export { telemetryFor } from "jsr:@fathym/common@0.2.307-common-release/telemetry";
 
-export { type EverythingAsCode } from "jsr:@fathym/eac@0.2.211-hmis";
-export { type EaCRuntimeContext } from "jsr:@fathym/eac@0.2.211-hmis/runtime";
+export { type EverythingAsCode } from "jsr:@fathym/eac@0.2.213-hmis";
+export { type EaCRuntimeContext } from "jsr:@fathym/eac@0.2.213-hmis/runtime";
 export {
   type EaCRuntimeConfig,
   type EaCRuntimePluginConfig,
-} from "jsr:@fathym/eac@0.2.211-hmis/runtime/config";
+} from "jsr:@fathym/eac@0.2.213-hmis/runtime/config";
 export {
   type EaCRuntimeHandler,
   type EaCRuntimeHandlers,
-} from "jsr:@fathym/eac@0.2.211-hmis/runtime/pipelines";
-export { type EaCRuntimePlugin } from "jsr:@fathym/eac@0.2.211-hmis/runtime/plugins";
+} from "jsr:@fathym/eac@0.2.213-hmis/runtime/pipelines";
+export { type EaCRuntimePlugin } from "jsr:@fathym/eac@0.2.213-hmis/runtime/plugins";
 export {
   type EaCAzureADProviderDetails,
   type EverythingAsCodeIdentity,
-} from "jsr:@fathym/eac-identity@0.0.61-eac-cascade";
+} from "jsr:@fathym/eac-identity@0.0.63-eac-cascade";
 
 export {
   type EaCApplicationProcessorConfig,
   type EaCProcessor,
   isEaCProcessor,
-} from "jsr:@fathym/eac-applications@0.0.315-mcp-processor/processors";
+} from "jsr:@fathym/eac-applications@0.0.317-mcp-processor/processors";
 export {
   type ProcessorHandlerResult,
   type ProcessorHandlerResolver,
-} from "jsr:@fathym/eac-applications@0.0.315-mcp-processor/runtime/processors";
+} from "jsr:@fathym/eac-applications@0.0.317-mcp-processor/runtime/processors";
 
-export { IoCContainer } from "jsr:@fathym/ioc@0.0.22-ioc-release";
+export { IoCContainer } from "jsr:@fathym/ioc@0.0.23-ioc-release";
 
 export {
   type AccountInfo,
